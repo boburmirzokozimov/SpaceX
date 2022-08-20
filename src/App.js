29ae55f2b6909b5baa24c79e0d844801826bf7c1
boburmirzokozimov <104490050+boburmirzokozimov@users.noreply.github.com>
2022-08-20 07:26:24 +0500

    Main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Header } from './components';
+import { Error, Homepage } from "./pages";
+
+function App() {
+  return <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App;
