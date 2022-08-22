bc28b1030e530175be5c5b2369b87b36aea8d719
boburmirzokozimov <104490050+boburmirzokozimov@users.noreply.github.com>
2022-08-22 11:54:04 +0500

    Changes from here

--- SpaceX/src/components/Header.js
+++ SpaceX/src/components/Header.js
@@ -1,25 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { SiSpacex } from "react-icons/si";
 
 const Header = () => {
-  return (
-    <header className="absolute flex items-center justify-between px-5 w-full ">
-      <div>
-        <Link to="/">
-          <SiSpacex className="text-white text-8xl"/>
-        </Link>
-      </div>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to='/capsules' className="text-white text-sm lg:text-lg">Capsules</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
